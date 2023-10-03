d29c0f63a1f1ac5288de1ba71fda1bbbf2dead39
Joshua Hutson <jrhut1355@gmail.com>
2023-10-03 16:04:14 -0400

    Starting Python and HTML scripts

---
+++ import.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@name: import.py
+@desc: this file imports a JSON library to import data from the Bureau of Labor Statistics API
+@date: October 3rd, 2023
+@author: Joshua Hutson
+"""
+
